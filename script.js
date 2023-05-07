b1cf57fda978d30e7f36d5ee0ed7039c39e3478a
Brandon Paulson <brandonpaulson28@gmail.com>
2023-05-07 16:17:47 -0500

    added in comments for better readability

--- script.js
+++ script.js
@@ -11,26 +11,21 @@ function clickedSubmit(event) {
         employeeInformationObject.annualSalary = (document.getElementById("SalaryInput").value);
         addNewTableRow(employeeInformationObject);
         addMonthlyCosts(employeeInformationObject);
-}
+};
 
 //function to append new table row
 function addNewTableRow(object) {
     document.getElementById("tableBody").innerHTML += `<tr class="newInfo"> <td> ${object.firstName}</td> <td>${object.lastName}</td> <td>${object.id}</td> <td>${object.title}</td> <td>${object.annualSalary}</td> <td><button onclick="deleteRow(event)"> Delete </button></td> </tr>`
-}
+};
 
 //function to delete row onclick
 function deleteRow(event) {
     event.target.parentElement.parentElement.remove();
-}
+};
 
+//Function for creating a sumation of salaries, and adding in a red background when value>20,000
 
 let totalSalaray = 0;
-// totalSalaray += document.getElementById("SalaryInput").value;
-// if(totalSalaray >= 20000){
-//     document.getElementById("totalCosts").style.backgroundColor= 'red';
-// }
-// console.log(document.getElementById("SalaryInput").value);
-
 
 function addMonthlyCosts(object) {
     let sum =  totalSalaray += Number(object.annualSalary);
@@ -39,4 +34,4 @@ function addMonthlyCosts(object) {
         document.getElementById("totalCosts").style.backgroundColor= 'red';
     }
 
-}
+};
