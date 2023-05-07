872a0c11b369d02e6bd744186e2a6ccc9b359b4b
Brandon Paulson <brandonpaulson28@gmail.com>
2023-05-07 10:03:13 -0500

    added in a new function for the remove row on delete click

--- script.js
+++ script.js
@@ -13,16 +13,13 @@ function clickedSubmit(event) {
 }
 
 function addNewTableRow(object) {
-    document.getElementById("tableBody").innerHTML += `<tr> <td> ${object.firstName}</td> <td>${object.lastName}</td> <td>${object.id}</td> <td>${object.title}</td> <td>${object.annualSalary}</td> <td><button onclick="deleteRow(event)"> Delete </button></td> </tr>`
+    document.getElementById("tableBody").innerHTML += `<tr class="newInfo"> <td> ${object.firstName}</td> <td>${object.lastName}</td> <td>${object.id}</td> <td>${object.title}</td> <td>${object.annualSalary}</td> <td><button onclick="deleteRow(event)"> Delete </button></td> </tr>`
 }
 
+// function deleteRow(event) {
+//     document.getElementById("newInfo").remove();
+// };
+
 function deleteRow(event) {
-    document.getElementById("tableBody").childElement.remove();
-};
-
-//This function works and adds in a new row but is all in one comma deliminated row
-// function addNewTableRow(object) {
-//     document.getElementById("tableBody").innerHTML = `<tr> ${object.firstName}, ${object.lastName}, ${object.id}, ${object.title}, ${object.annualSalary} </tr>`
-// }
-
-//Headers.set(id="FirstName", object.firstName), I think I need to use this to append to the table
+    event.target.parentElement.parentElement.remove();
+}
